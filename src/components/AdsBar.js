667fabb1d3cce2be8cd0e08dc2ef46bbb64654f1
John Best <johnfbest@gmail.com>
2022-01-28 14:48:23 -0600

    squash this commit

--- src/components/AdsBar.js
+++ src/components/AdsBar.js
@@ -16,10 +16,6 @@ class AdsBar extends Component {
         </a>
       </div>
       <div className="AdsBar">
-
-        {/* <div>
-          <h2 style={{Color: '#fff'}}>AdsBar</h2>
-        </div> */}
         <ins
           className="adsbygoogle douhave-google-ad"
           /*
@@ -30,24 +26,6 @@ class AdsBar extends Component {
           data-ad-format="auto"
           data-full-width-responsive="true"
         />
-        {/* <ins className="adsbygoogle douhave-google-add"
-          style={{"display": "block", "marginTop": "80px", "marginBottom": "30px", border:"1px solid #1d1d1d"}}
-          data-ad-client="ca-pub-3613438433904573"
-          data-ad-slot="5999152125"
-          data-ad-format="auto"></ins> */}
-
-        {/* <ins className="adsbygoogle douhave-google-add"
-          style={{"display": "block", "marginTop": "80px", "marginBottom": "30px", border:"1px solid #1d1d1d"}}
-          data-ad-client="ca-pub-3613438433904573"
-          data-ad-slot="5999152125"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins> */}
-        {/* <ins className="adsbygoogle douhave-google-add"
-          style={{"display": "block", "marginTop": "80px", "marginBottom": "30px", border:"1px solid #1d1d1d"}}
-          data-ad-client="ca-pub-3613438433904573"
-          data-ad-slot="5999152125"
-          data-ad-format="auto"
-          data-full-width-responsive="true"></ins> */}
       </div>
       </>
     );
